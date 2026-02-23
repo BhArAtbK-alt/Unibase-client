0321a17bbd4f6e41ed8268c687f5b978b44af371
Omkar Ugalmugale <omkarugalmugale84@gmail.com>
2026-02-23 19:47:41 +0530

    Delete Backend directory

--- Backend/config/pg.js
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-import 'dotenv/config';
-
-const pool = new Pool({
-    connectionString: process.env.PG_URL,
-    rejectUnauthorized: false
-});
-
-export default pool;
--- Backend/utils/createApiKey.js
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export const generateApiKey = () => {
-    
-    const randomHex = crypto.randomBytes(20).toString('hex');
-    return `ub_live_${randomHex}`;
-};
