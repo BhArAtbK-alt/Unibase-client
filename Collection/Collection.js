5b4b407f826aa2369716aaa0f2fdf97c74ea81b5
OmkarDev28 <omkarugalmugale84@gmail.com>
2026-03-08 19:22:00 +0530

    updates

--- Collection/Collection.js
+++ Collection/Collection.js
@@ -4,30 +4,22 @@ export class Collection {
         this.client = client;
     }
 
-    /**
-     * get() -> gets all documents
-     * get(field, operator, value) -> gets filtered documents
-     */
+
     async get(field = null, operator = null, value = null) {
         const payload = {
             collection: this.collectionName,
         };
 
-        // If arguments are provided, it's a conditional query
         if (field && operator && value !== null) {
             payload.action = 'query_docs';
             payload.filter = { field, operator, value };
         } else {
-            // Otherwise, get everything
             payload.action = 'get_all_docs';
         }
 
         return this.client.sendDocReq(payload);
     }
 
-    /**
-     * Get a single document by ID
-     */
     async findById(docId) {
         return this.client.sendDocReq({
             action: 'get_doc_by_id',
